d2e7382f73b88b398692c905c6d03b3de5fc3a6a
eremophobia <succducc0@gmail.com>
2022-07-20 01:02:53 +0200

    - API changes + hyperlinks

--- commands/main/user.js
+++ commands/main/user.js
@@ -3,11 +3,17 @@ module.exports = ({
     name: "user",
     code: `
     $title[$api[https://api.mojang.com/users/profiles/minecraft/$message[];name]'s UUID.]
+    $thumbnail[https://minotar.net/avatar/$message[]]
     
-    $thumbnail[http://cravatar.eu/avatar/$message[]/64.png]
-    $description[$api[https://api.mojang.com/users/profiles/minecraft/$message[];id]]
-    
+    $description[
+                $api[https://api.mojang.com/users/profiles/minecraft/$message[];id]
+
+                $addField[Links;
+                    {hyper:Download Skin:https://minotar.net/download/$message[]}
+                    {hyper:NameMC:https://it.namemc.com/profile/$message[]}
+                    {hyper:Plancke.io:https://plancke.io/hypixel/player/stats/$message[]}]
+    ]
+
     $color[#123123]
     `
-
 })
